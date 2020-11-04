0801ad44bf355067d68aad08afa8be5473f84048
Echo Li <ec.li@mail.utoronto.ca>
2020-11-04 16:59:18 +0100

    Added all UserController subclasses and associated method signatures to all classes

---
+++ phase1/src/SpeakerController.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpeakerController extends UserController {
+    public SpeakerController(EventManager em, UserManager um, RoomManager rm, MessageManager mm, int userId) {
+        super(em, um, rm, mm, userId);
+    }
+
+    public boolean messageEventsAttendees(List<Integer> eventIds) {
+        return true;
+    }
+
+    public boolean messageEventAttendees(int eventId) {
+        return true;
+    }
+
+    public List<String> getSpeakerEvents() {
+        return new ArrayList<>();
+    }
+}
--- phase1/src/UserController.java
+++ phase1/src/UserController.java
@@ -1,2 +1,46 @@
-public class UserController {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class UserController {
+    private EventManager em;
+    private UserManager um;
+    private RoomManager rm;
+    private MessageManager mm;
+    private int userId;
+
+    public UserController(EventManager em, UserManager um, RoomManager rm, MessageManager mm, int userId) {
+        this.em = em;
+        this.um = um;
+        this.rm = rm;
+        this.mm = mm;
+        this.userId = userId;
+    }
+
+    public boolean signUpEventAttendance(int eventId) {
+        return true;
+    }
+
+    public boolean cancelEventAttendance(int eventId) {
+        return true;
+    }
+
+    public String getAttendingEvents() {
+        return "";
+    }
+
+    public String getAllEvents() {
+        return "";
+    }
+
+    public List<String> getMessages() {
+        return new ArrayList<>();
+    }
+
+    public boolean messageSingleAttendee(int recipientId) {
+        return true;
+    }
+
+    public boolean messageSingleSpeaker(int recipientId) {
+        return true;
+    }
 }
