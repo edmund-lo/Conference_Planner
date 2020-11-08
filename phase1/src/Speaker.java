e943a94d1ef102d48b088635284d1a3d0df03436
Edmund <edmun.lo@mail.utoronto.ca>
2020-11-08 16:47:20 -0500

    Finish User, Attendee, Organizer, Speaker class

--- phase1/src/Speaker.java
+++ phase1/src/Speaker.java
@@ -1,3 +1,38 @@
-public class Speaker extends User{
+import java.time.LocalDateTime;
+import java.util.HashMap;
 
+public class Speaker extends User{
+    private HashMap<String, LocalDateTime[]> speakerSchedule;
+
+    public Speaker(String username, String password) {
+        super(username, password);
+        this.speakerSchedule = new HashMap<>();
+    }
+
+    public HashMap<String, LocalDateTime[]> getSpeakerSchedule() {
+        return speakerSchedule;
+    }
+
+    public void addSpeakerEvent(String eventID, LocalDateTime startTime, LocalDateTime endTime) {
+        LocalDateTime[] time = {startTime, endTime};
+        speakerSchedule.put(eventID, time);
+    }
+
+    public void cancelSpeakerEvent(String eventID) {
+        speakerSchedule.remove(eventID);
+    }
+
+    public boolean canAddSpeakerEvent(String eventID, LocalDateTime startTime, LocalDateTime endTime) {
+        if (speakerSchedule.containsKey(eventID)) {
+            return false;
+        } else {
+            for (LocalDateTime[] time : speakerSchedule.values()) {
+                if ((time[0].isBefore(startTime) && time[1].isAfter(startTime)) ||
+                        (time[0].isBefore(endTime) && time[1].isAfter(endTime))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
