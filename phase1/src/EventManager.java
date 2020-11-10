85da39f229000e616aee75fd0d53a03f7d765d91
Tony <tonyl.lin@mail.utoronto.ca>
2020-11-10 15:36:27 -0500

    Implemented some methods in UserController and added two methods in eventManager

--- phase1/src/EventManager.java
+++ phase1/src/EventManager.java
@@ -1,5 +1,8 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
+
 public class EventManager{
     private HashMap<String, Event> allEvents;
 
@@ -11,8 +14,8 @@ public class EventManager{
         return allEvents.containsKey(eventID);
     }
 
-    public void createNewEvent(String eventName, String roomID,LocalDateTime startTime, LocalDateTime endTime, String speakerID){
-        Event newEvent = new Event(UUID.randomUUID().toString(), roomID, eventName, startTime, endTime, speakerID)
+    public void createNewEvent(String eventName, String roomID, LocalDateTime startTime, LocalDateTime endTime, String speakerID){
+        Event newEvent = new Event(UUID.randomUUID().toString(), roomID, eventName, startTime, endTime, speakerID);
         allEvents.put(newEvent.getEventID(), newEvent);
     }
     public boolean removeEvent(String eventID){
@@ -25,28 +28,38 @@ public class EventManager{
     }
     public boolean changeEventTime(String eventID, LocalDateTime startTime, LocalDateTime endTime){
         if(eventExists(eventID)){
-            allEvents.get(eventID).changeTime(startTime, endTime)
+            allEvents.get(eventID).changeTime(startTime, endTime);
             return true;
         }
         else{
             return false;
         }
     }
-    public boolean addUserToEvent(String eventID, String userID){
-        currentEvent = allEvents.get(eventID);
+    public boolean canAddUserToEvent(String eventID, String userID){
+        Event currentEvent = allEvents.get(eventID);
         if(eventExists(eventID) && currentEvent.getAttendingUsers().size() < 2){
-            return currentEvent.addUserToEvent(userID);
+            return true;
         }else{
             return false;
         }
     }
+    public boolean addUserToEvent(String eventID, String userID){
+        Event currentEvent = allEvents.get(eventID);
+        return currentEvent.addUserToEvent(userID);
+    }
     public boolean removeUserFromEvent(String eventID, String userID){
-        currentEvent = allEvents.get(eventID);
+        Event currentEvent = allEvents.get(eventID);
         if(eventExists(eventID)){
             return currentEvent.removeUserFromEvent(userID);
         }else{
             return false;
         }
     }
+    public Event getEventById(String eventID) {
+        return allEvents.get(eventID);
+    }
 
+    public HashMap<String, Event> getAllEvents() {
+        return allEvents;
+    }
 }
