d39a58077b61d2e9eef799683406d889ae281069
Sam De Abreu <sam.deabreu@mail.utoronto.ca>
2020-11-10 16:30:15 -0500

    Implemented toString method for Event, canAddSpeakerToEvent and addSpeakerToEvent to EventManager and added Javadoc for Event and EventManager

--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -1,6 +1,9 @@
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * Implementation of an event with all details pretaining to it stored inside.
+ */
 public class Event {
     private String eventName;
     private String eventID;
@@ -10,41 +13,98 @@ public class Event {
     private LocalDateTime endTime;
     private List<String> attendingUsers;
 
+    /**
+     * Constructor for Event
+     *
+     * @param eventID The randomly generated ID for this Event
+     * @param roomID The ID of the room to which the Event will be held in
+     * @param eventName Name of the event
+     * @param startTime Start time of the event
+     * @param endTime End time of the event
+     */
     public Event(String eventID, String roomID, String eventName, LocalDateTime startTime,
-                 LocalDateTime endTime, String speakerID){
-        this.eventiD = eventID;
+                 LocalDateTime endTime){
+        this.eventID = eventID;
         this.roomID = roomID;
         this.eventName = eventName;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.speakerID = speakerID;
         attendingUsers = new ArrayList<String>();
     }
 
+    /**
+     * Getter for ID of the speaker
+     *
+     * @return The ID of the speaker speaking at this event
+     */
     public String getSpeakerID() {
         return speakerID;
     }
 
+    /**
+     * getter for ID of the event
+     *
+     * @return The ID of this event
+     */
     public String getEventID() {
         return eventID;
     }
 
+    /**
+     * getter for the list of attending users
+     *
+     * @return An arraylist with IDs of all users attending this event
+     */
     public List<String> getAttendingUsers() {
         return attendingUsers;
     }
 
+    /**
+     * getter for the name of this event
+     *
+     * @return the name of this event
+     */
     public String getEventName() {
         return eventName;
     }
 
+    /**
+     * getter for the start time of this event
+     *
+     * @return the start time of this event
+     */
     public LocalDateTime getStartTime() {
         return startTime;
     }
 
+    /**
+     * getter for the end time of this event
+     *
+     * @return the end time of this event
+     */
     public LocalDateTime getEndTime() {
         return endTime;
     }
 
+    /**
+     * the toString method for Event
+     *
+     * @return a String representation of Event that contains the event name, time and number of attending users
+     */
+    public String toString(){
+        return "Event Name: "+this.eventName+"\n" +
+                "Time: "+String.valueOf(this.startTime.getHour())+" to "+String.valueOf(this.endTime.getHour())+" on "+
+                String.valueOf(this.startTime.getDayOfMonth())+"/"+String.valueOf(this.startTime.getMonthValue())+"\n" +
+                "# of Attending Users: "+String.valueOf(this.attendingUsers.size());
+    }
+
+    /**
+     * Adds the user with ID userID to the attending users list for this event. The user must not already be in the
+     * attending users list.
+     *
+     * @param userID the ID of the user that wants to be added to this event
+     * @return True iff the user was added successfully and false if not
+     */
     public boolean addUserToEvent(String userID){
         if(!attendingUsers.contains(userID)){
             attendingUsers.add(userID);
@@ -54,6 +114,14 @@ public class Event {
         }
 
     }
+
+    /**
+     * Removes the user with ID userID from the attending users list for this event. The user must be in the attending
+     * users list.
+     *
+     * @param userID the ID of the user taht wants to be removed from this event
+     * @return True iff the user was removed successfully and false if not
+     */
     public boolean removeUserFromEvent(String userID){
         if(attendingUsers.contains(userID)){
             attendingUsers.remove(userID);
@@ -64,10 +132,25 @@ public class Event {
         }
 
     }
+
+    /**
+     * Changes the time of when the event occurs. That is, changing the start and end time of the event.
+     *
+     * @param startTime the new start time of the event
+     * @param endTime the new end time of the event
+     */
     public void changeTime(LocalDateTime startTime, LocalDateTime endTime){
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
+    /**
+     * setter for the speakerID
+     *
+     * @param speakerID the ID of the speaker that wants to be added to Event
+     */
+    public setSpeaker(String speakerID){
+        this.speakerID = speakerID;
+    }
 
 }
--- phase1/src/EventManager.java
+++ phase1/src/EventManager.java
@@ -1,20 +1,50 @@
 import java.util.HashMap;
 import java.util.UUID;
+
+/**
+ * Helper that manages all interaction with the Event classes and ensures no rules are broken.
+ */
 public class EventManager{
     private HashMap<String, Event> allEvents;
 
+    /**
+     * Constructor for EventManager
+     *
+     * @param allEvents A hashmap where the values are events and the keys are the IDs to each respective Event
+     */
     public EventManager(HashMap<String, Event> allEvents){
         this.allEvents = allEvents;
     }
 
+    /**
+     * A helper to check if the event is in allEvents
+     *
+     * @param eventID the ID of the event we are checking
+     * @return True iff the event is in allEvents
+     */
     private boolean eventExists(String eventID){
         return allEvents.containsKey(eventID);
     }
 
-    public void createNewEvent(String eventName, String roomID,LocalDateTime startTime, LocalDateTime endTime, String speakerID){
-        Event newEvent = new Event(UUID.randomUUID().toString(), roomID, eventName, startTime, endTime, speakerID)
+    /**
+     * Creator for a new event
+     *
+     * @param eventName the name of the event
+     * @param roomID the ID of the room that the event is held in
+     * @param startTime the start time of the event
+     * @param endTime the end time of the event
+     */
+    public void createNewEvent(String eventName, String roomID, LocalDateTime startTime, LocalDateTime endTime){
+        Event newEvent = new Event(UUID.randomUUID().toString(), roomID, eventName, startTime, endTime)
         allEvents.put(newEvent.getEventID(), newEvent);
     }
+
+    /**
+     * remove event with ID eventID from allEvents
+     *
+     * @param eventID the ID of the event that wishes to be removed
+     * @return True iff the event was removed successfully
+     */
     public boolean removeEvent(String eventID){
         if(eventExists(eventID)){
             allEvents.remove(eventID);
@@ -23,6 +53,15 @@ public class EventManager{
             return false;
         }
     }
+
+    /**
+     * changes the time at which the event is held. That is, changes the start and end time of the event
+     *
+     * @param eventID the ID of the event that wishes to have its time changed
+     * @param startTime the new start time of the event
+     * @param endTime the new end time of the event
+     * @return True iff the time was changed successfully
+     */
     public boolean changeEventTime(String eventID, LocalDateTime startTime, LocalDateTime endTime){
         if(eventExists(eventID)){
             allEvents.get(eventID).changeTime(startTime, endTime)
@@ -32,6 +71,14 @@ public class EventManager{
             return false;
         }
     }
+
+    /**
+     * adds user with ID userID to event with ID eventID. The event must have enough capacity and the event must exist
+     *
+     * @param eventID the ID of the event
+     * @param userID the ID of the user
+     * @return True iff the user with ID userID was added successfully to event with ID eventID
+     */
     public boolean addUserToEvent(String eventID, String userID){
         currentEvent = allEvents.get(eventID);
         if(eventExists(eventID) && currentEvent.getAttendingUsers().size() < 2){
@@ -40,6 +87,14 @@ public class EventManager{
             return false;
         }
     }
+
+    /**
+     * removes user with ID userID from event with ID eventID. The event must exist
+     *
+     * @param eventID the ID of the event
+     * @param userID the ID of the user
+     * @return True iff the user with ID userID was removed successfully from event with ID eventID
+     */
     public boolean removeUserFromEvent(String eventID, String userID){
         currentEvent = allEvents.get(eventID);
         if(eventExists(eventID)){
@@ -47,6 +102,32 @@ public class EventManager{
         }else{
             return false;
         }
+    }
+
+    /**
+     * checks to see if speaker with ID speakerID can be added to event with ID eventID
+     *
+     * @param speakerID the ID of the speaker
+     * @param eventID the ID of the event
+     * @return True iff the speaker was successfully added to the event with ID eventID
+     */
+    public boolean canAddSpeakerToEvent(String speakerID, String eventID){
+        if(eventExists(eventID) && allEvents.get(eventID).getSpeakerID() == null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * adds speaker with ID speakerID to event with ID eventID
+     *
+     * @param speakerID ID of the speaker
+     * @param eventID ID of the event
+     */
+    public addSpeakerToEvent(String speakerID, String eventID){
+        allEvents.get(eventID).setSpeaker(speakerID);
     }
 
 }
