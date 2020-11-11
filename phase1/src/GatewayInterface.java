f08ab7a43c9340a34dbde642e21f8f6a4e714b95
dylanbaptist <dylan.baptist@mail.utoronto.ca>
2020-11-11 15:48:25 -0500

    preliminary code with abstract methods needed for gateway implementation

---
+++ phase1/src/GatewayInterface.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.List;
+
+public interface GatewayInterface extends Serializable {
+    /**
+     * This is a gateway interface that interacts with all other gateway classes
+     * GatewayInterface has a static final variable directoryName and extends Serializable
+     * There are two methods, serializeData which serializes the inputted parameter
+     * using Serializable and returns none.
+     * Method deserializeData unpacks a given serialized parameter producing
+     * an object of List<T>
+     */
+
+    //group_0143/phase1/src
+
+    String directoryName = "https://markus.teach.cs.toronto.edu/git/csc207-2020-09/group_0143";
+    void serializeData () throws FileNotFoundException;
+    abstract <T> List<T> unserializeData();
+
+}
+
+
