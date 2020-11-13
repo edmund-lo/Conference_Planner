a9d5be07fd69139eccc146103dbaa9e808448f01
dylanbaptist <dylan.baptist@mail.utoronto.ca>
2020-11-13 17:31:24 -0500

    changed numbering for display options, and added displayMenu

--- phase1/src/SpeakerPresenter.java
+++ phase1/src/SpeakerPresenter.java
@@ -3,4 +3,17 @@ public abstract class SpeakerPresenter {
     public void writeMessageToAttendees() {
         System.out.println("Enter the message that you would like to send to the Attendees: ");
     }
+
+    public static void displayMenu(){
+        System.out.println("Select Option " +
+                "\n 0. Logout" +
+                "\n 1. Sign Up for Events" +
+                "\n 2. Cancel Events" +
+                "\n 3. Message Menu" +
+                "\n 4. Message All Attendees" +
+                "\n 5. Message All Speakers" +
+                "\n 6. Schedule a Speaker" +
+                "\n 7. Create a Room");
+    }
+
 }
