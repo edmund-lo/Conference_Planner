2754af1db44b044d257322e8930c5d8afe1946e8
dylanbaptist <dylan.baptist@mail.utoronto.ca>
2020-11-13 17:17:32 -0500

    first version of SpeakerPresenter

--- phase1/src/SpeakerPresenter.java
+++ phase1/src/SpeakerPresenter.java
@@ -1,4 +1,5 @@
 public abstract class SpeakerPresenter {
+    //writes messages to attendees
     public void writeMessageToAttendees() {
         System.out.println("Enter the message that you would like to send to the Attendees: ");
     }
