61a0928db649891191b8858374bfa70b313257e6
dylanbaptist <dylan.baptist@mail.utoronto.ca>
2020-11-13 13:39:00 -0500

    added showMessages statement MessagePresenter

--- phase1/src/MessagePresenter.java
+++ phase1/src/MessagePresenter.java
@@ -2,6 +2,7 @@ public abstract class MessagePresenter extends LoginPresenter {
     public void writeMessage() {
         System.out.println("Write your message here: ");
     }
+    //displays menu options
     public void displayMessageOptions() {
         System.out.println("Press \"1\" if you would like to display all messages. " +
                 "\n Press \"2\" if you would like to send a message. \n If not, press enter to continue. ");
@@ -9,7 +10,12 @@ public abstract class MessagePresenter extends LoginPresenter {
     //This needs work, determine if separate method to send to all Speakers and Attendees
     public void specifyReceiverOfMessage() {
         System.out.println("Enter \"1\" if you would like to send a message to a Speaker. " +
-                "\n Press \"2\" to send a message to an Attendee." +
-                "\n Press \"3\" to send a message to All Speakers. \n Press \"4\" to send a message to All Attendees.");
+                "\nPress \"2\" to send a message to an Attendee." +
+                "\nPress \"3\" to send a message to All Speakers. \nPress \"4\" to send a message to All Attendees.");
     }
+    //display messages
+    public void showMessages() {
+        System.out.println("Here are your messages.");
+    }
+
 }
