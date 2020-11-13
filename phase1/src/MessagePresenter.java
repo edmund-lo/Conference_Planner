d3afd1e4a6cfd8587e9d0d4511e93a47ac49fd0c
dylanbaptist <dylan.baptist@mail.utoronto.ca>
2020-11-13 12:37:10 -0500

    revised to display message options

--- phase1/src/MessagePresenter.java
+++ phase1/src/MessagePresenter.java
@@ -2,8 +2,9 @@ public abstract class MessagePresenter extends LoginPresenter {
     public void writeMessage() {
         System.out.println("Write your message here: ");
     }
-    public void confirmDisplayMessages() {
-        System.out.println("Press \"1\" if you would like to display all messages. \n If not, press enter to continue");
+    public void displayMessageOptions() {
+        System.out.println("Press \"1\" if you would like to display all messages. " +
+                "\n Press \"2\" if you would like to send a message. \n If not, press enter to continue. ");
     }
     //This needs work, determine if separate method to send to all Speakers and Attendees
     public void specifyReceiverOfMessage() {
