32d36b7b506e50030875df5db68707e09f0545b6
keeganmcgonigal <keegan.mcgonigal@mail.utoronto.ca>
2020-11-15 16:10:36 -0500

    Finished javadoc for LoginPresenter

--- phase1/src/Presenters/LoginPresenter.java
+++ phase1/src/Presenters/LoginPresenter.java
@@ -1,7 +1,7 @@
 package Presenters;
 
 /**
- * A Presenter class that provides output to the user at time of login
+ * A Presenter class that provides output to the user at time of login.
  */
 
 public class LoginPresenter {
@@ -12,11 +12,17 @@ public class LoginPresenter {
         System.out.println("Username is taken! \nType \"login\" to login if account already exists and you wish to " +
                 "login. \nIf not, press enter to try a different username");
     }
-    //prompts username request
+
+    /**
+     * Asks the user to type in a username.
+     */
     public void EnterUsername(){
         System.out.println("Enter Username:");
     }
-    //prompts type of Account
+
+    /**
+     * Asks the user to enter the type of account to be created.
+     */
     public void AccountType(){
         System.out.println("What is the account type? Press:" +
                 "\n1. for Organizer" +
@@ -24,30 +30,49 @@ public class LoginPresenter {
                 "\n3. for Speaker");
     }
     //Outputs request for password
+    /**
+     * Asks the user to type in a password.
+     */
     public void EnterPassword(){
         System.out.println("Enter Your Password:");
     }
-    //Prints if incorrect credentials are provided
+    /**
+     * Lets user know that the username or password that they entered was incorrect.
+     */
     public void IncorrectCredentials(){
         System.out.println("Incorrect Username or Password.");
     }
-    //Create new account
+
+    /**
+     * Asks the user to enter "New" if they would like to create a new account.
+     */
     public void New() {
         System.out.println("Type \"New\" to create new account. Press enter to continue to the login screen");
     }
-    //If invalid number is entered, prints statement
+
+    /**
+     * Asks the user to enter a valid number if the input is not a valid option for them.
+     */
     public void ValidNumber() {
         System.out.println("Please enter a valid number.");
     }
-    //Print at login
+
+    /**
+     * Prints login message.
+     */
     public void Login() {
         System.out.println("Login:");
     }
 
+    /**
+     * Prints create account message.
+     */
     public void CreateAccountP() {
         System.out.println("Create Account");
     }
-
+    /**
+     * Lets the user know that their account has been successfully created.
+     */
     public void AccountMade() {
         System.out.println("Account has been made! Return to the login screen to login to your account.");
     }
