29a67a0b742537a7c7626873f18091f9685517ba
Sam De Abreu <sam.deabreu@mail.utoronto.ca>
2020-11-15 15:41:48 -0500

    Added Javadoc to OrganizerPresenter and SpeakerPresenter

--- phase1/src/Presenters/SpeakerPresenter.java
+++ phase1/src/Presenters/SpeakerPresenter.java
@@ -8,29 +8,50 @@ package Presenters;
  * confirms and rejects success of sending messages to event attendees based on invalid credentials
  */
 public class SpeakerPresenter extends UserPresenter {
-    //displays numbered menu options
+    /**
+     * Outputs the main menu display. Note that it is the same as the user menu with 2 extra options
+     */
     public void displayMenu(){
         super.displayMenu();
         System.out.println(" 5. Message Attendees at Events as Speaker" +
-                "\n 6. Show Events as Speaker");
+                "\n 6. Show Events you are speaking at");
     }
-    //Prints statement introducing list of events
+
+    /**
+     * Outputs label for introducing list of events speaker is speaking at
+     */
     public void speakerEventsLabel() {
         System.out.println("Here are a list of events you are speaking at:");
     }
-    //Prints for user to see that event numbers are required
+
+    /**
+     * Outputs label for instructions on how to input event numbers for messaging Event attendees
+     */
     public void messageEventAttendeesPrompt() {
         System.out.println("Enter the event numbers separated by a comma:");
     }
-    //Confirms success of messaging attendees
+
+    /**
+     * Outputs confirmation success of sending message to attendees of event name event
+     *
+     * @param event name of the event
+     */
     public void messageEventAttendeesResult(String event) {
         System.out.println("Successfully sent message to attendees of " + event + ".");
     }
-    //If invalid event provided
+
+    /**
+     * Outputs label that indicates error for an invalid event number
+     */
     public void invalidEventNumberError() {
         System.out.println("Event number is formatted incorrectly!");
     }
-    //If messages aren't successfully sent
+
+    /**
+     * Outputs error label for failing to message all attendees of event name event
+     *
+     * @param event name of event
+     */
     public void messageEventAttendeesError(String event) {
         System.out.println("Unable to message attendees of " + event + "!");
     }
