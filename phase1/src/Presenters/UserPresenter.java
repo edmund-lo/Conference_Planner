065125b99507b5789a8ebdcc5832d6967d7593ed
dylanbaptist <dylan.baptist@mail.utoronto.ca>
2020-11-15 16:27:27 -0500

    added more user friendly interaction and functionality in display menu, and specifies type of the user, spacing

--- phase1/src/Presenters/UserPresenter.java
+++ phase1/src/Presenters/UserPresenter.java
@@ -5,6 +5,9 @@ import UseCases.UserManager;
 import Controllers.UserController;
 /**
  * Presenter outputs for client type User
+ * Displays menu options, prompts to signup or cancel an event
+ * Prompts message options
+ *
  */
 
 public class UserPresenter {
@@ -12,35 +15,61 @@ public class UserPresenter {
     // go back, sign up for an event with a number
     // cancel an event with a known number
     // message a user, view list of messages
+
+    /**
+     * Displays menu options for a logged-in user based on their capabilities.
+     * @param s string to specify type of user at sign-in period
+     * @param username the username of the given user
+     */
     public void displayMenu(String s, String username) {
-        System.out.println("Hello, " + username + "(" + s + ")!\nSelect Option " +
+        System.out.println("Hello, " + username + " (" + s + ")!\nSelect Option " +
                 "\n 0. Logout" +
                 "\n 1. Sign Up for Events" +
                 "\n 2. Cancel Attendance from Events" +
                 "\n 3. Message Users / View Messages" +
                 "\n 4. View all events you're attending");
     }
+
+    /**
+     * Outputs prompt for signup for an event
+     */
     //Numbered options during signup
     public void signUpEventPrompt() {
         System.out.println("Enter 0 to go back or enter a number to select an event to sign up for:");
     }
+
+    /**
+     * Outputs prompt for cancelling event
+     */
     //prompts user option for cancelling event
     public void cancelEventPrompt() {
         System.out.println("Enter a number to select an event to cancel:" +
                 "\n Press 0 to go back.");
     }
+
+    /**
+     * Outputs prompt for menu options in regards to messaging.
+     */
     //prompt for messaging options
     public void messageMenuPrompt() {
-        System.out.println("Press :" +
+        System.out.println("Enter :" +
                 "\n 0. Go back" +
                 "\n 1. Message a user" +
                 "\n 2. View the messages you sent" +
                 "\n 3. View the messages you received");
     }
+
+    /**
+     * Prompts user to specify receiver
+     */
     //prompt for receiver option
     public void enterReceiverPrompt() {
         System.out.println("Enter the user you wish to message:");
     }
+
+    /**
+     *
+     */
     //prompt for writing message
     public void enterMessagePrompt() {
         System.out.println("Enter the message you wish to send:");
@@ -54,7 +83,7 @@ public class UserPresenter {
         System.out.println("Here is a list of all the events you are attending:");
     }
     public void exitlistAllEventsLabel(){
-        System.out.println("Press 0 to go back");
+        System.out.println("Enter 0 to go back");
     }
     //Introduces list of events that can be cancelled
     public void cancelEventListLabel() {
