b317e452813a9e50a78aa2b176a073b1de5ca3b4
Edmund <edmun.lo@mail.utoronto.ca>
2020-11-16 16:27:53 -0500

    fix canAddSpeakerEvent in speaker

--- phase1/src/Entities/Speaker.java
+++ phase1/src/Entities/Speaker.java
@@ -70,6 +70,8 @@ public class Speaker extends User implements Serializable {
                 if ((time[0].isBefore(startTime) && time[1].isAfter(startTime)) ||
                         (time[0].isBefore(endTime) && time[1].isAfter(endTime))) {
                     return false;
+                } else if (time[0].isEqual(startTime)) {
+                    return false;
                 }
             }
             return true;
