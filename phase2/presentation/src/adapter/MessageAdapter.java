8597e791215990b5ed0f0dde835acce8128aca09
Wei Gao <wei.gao@mail.utoronto.ca>
2020-12-06 05:43:10 +0800

    delete and message presenters implementation

--- phase2/presentation/src/adapter/MessageAdapter.java
+++ phase2/presentation/src/adapter/MessageAdapter.java
@@ -1,4 +1,39 @@
 package adapter;
 
+import model.Message;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import util.DateTimeUtil;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
 public class MessageAdapter {
+    private static final MessageAdapter Instance = new MessageAdapter();
+
+    public static MessageAdapter getInstance() { return Instance; }
+
+    private MessageAdapter() {}
+
+    public List<Message> adaptData(JSONArray data) {
+        List<Message> message = new ArrayList<>();
+        for (Object datum : data) {
+            JSONObject jsonObject = (JSONObject) datum;
+            message.add(mapMessage(jsonObject));
+        }
+        return message;
+    }
+
+    private Message mapMessage(JSONObject jsonObject) {
+        String senderName = String.valueOf(jsonObject.get("senderName"));
+        List<String> recipientNames = List<String>.valueOf(jsonObject.get("recipientNames"));
+        String content = String.valueOf(jsonObject.get("content"));
+        boolean read = jsonObject.get("read").equals("true");
+        LocalDateTime messageTime = DateTimeUtil.getInstance().parse(String.valueOf(jsonObject.get("messageTime")));
+
+        return new Message(senderName, recipientNames, content, read, messageTime);
+    }
 }
--- phase2/presentation/src/admin/IDeleteMessagesPresenter.java
+++ phase2/presentation/src/admin/IDeleteMessagesPresenter.java
@@ -1,6 +1,15 @@
 package admin;
 
 import common.IPresenter;
+import javafx.event.ActionEvent;
+import model.Message;
+
+import java.util.List;
 
 public interface IDeleteMessagesPresenter extends IPresenter {
+    void removeButtonAction(ActionEvent actionEvent);
+    void setResult(String result);
+    List<Message> getMessages();
+    void displayMessages(List<Message> messages);
+    void displayMessageDetails(Message message);
 }
