bd167e31c65f4a5db933f2393def4a4e2e924730
dylanbaptist <dylan.baptist@mail.utoronto.ca>
2020-12-07 10:02:41 -0500

    completed getAccountInfo and getUserAccount

--- phase2/core/src/usecases/UserAccountManager.java
+++ phase2/core/src/usecases/UserAccountManager.java
@@ -1,8 +1,12 @@
 package usecases;
 
 import entities.UserAccountEntity;
+
+import java.lang.reflect.Array;
 import java.util.*;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.ArrayList;
 import org.json.simple.*;
 
 /**
@@ -15,7 +19,8 @@ public class UserAccountManager implements Serializable {
     // key value pairing with username and all the properties of a UserAccountEntity
     // such as security questions, username, password, etc.
     private HashMap<String, UserAccountEntity> allUserAccountEntities;
-    ArrayList<String> logInfo = new ArrayList<>();
+    ArrayList<String[]> logInfo = new ArrayList<>();
+    ArrayList<UserAccountEntity> logEntity = new ArrayList<>();
     /**
      * Constructor for UserAccountManager object. Initializes empty HashMap.
      *
@@ -32,9 +37,23 @@ public class UserAccountManager implements Serializable {
         return null;
     }
 
-//    public ArrayList<String[]> getAccountInfo() {
-//        logInfo.
-//        logInfo.add(this.);
-//        logInfo
-//    }
+    public ArrayList<String[]> getAccountInfo() {
+        //iterate through the keys of the UAM,
+        //for each key, add the username and password as the two index in the array of strings
+//        String[] temp;
+//        String[] temp1;
+        String getUsername1 = "";
+        String getPassword1 = "";
+        for(String i : allUserAccountEntities.keySet()) {
+            logEntity.add(this.allUserAccountEntities.get(i));
+
+        }
+        for(int i = 0; i < logEntity.size(); i++) {
+            getUsername1= logEntity.get(i).getUsername();
+            getPassword1 = logEntity.get(i).getPassword();
+
+            logInfo.add(i,new String[]{getUsername1, getPassword1});
+        }
+        return logInfo;
+    }
 }
