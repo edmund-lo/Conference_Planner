a42d7ef02321626acc2ffc182a903c341f8c5671
Echo Li <ec.li@mail.utoronto.ca>
2020-12-11 20:48:27 +0100

    Fixed ComboBox default value, added JavaDoc to RegisterPresenter

--- phase2/presentation/src/login/impl/RegisterPresenter.java
+++ phase2/presentation/src/login/impl/RegisterPresenter.java
@@ -8,21 +8,36 @@ import org.json.simple.JSONObject;
 import util.ComponentFactory;
 import util.TextResultUtil;
 
+/**
+ * Presenter class for the register new user screen
+ */
 public class RegisterPresenter implements IRegisterPresenter {
     private final IRegisterView view;
     private final LoginController lc;
 
+    /**
+     * Initialises a RegisterPresenter object with given view and new LoginController
+     * @param view IRegisterView interface implementation
+     */
     public RegisterPresenter(IRegisterView view) {
         this.view = view;
         this.lc = new LoginController();
         init();
     }
 
+    /**
+     * Performs back button action and changes scene to login screen
+     * @param actionEvent JavaFX ActionEvent object representing the event of the button press
+     */
     @Override
     public void backButtonAction(ActionEvent actionEvent) {
         ComponentFactory.getInstance().createLoggedOutComponent(actionEvent, "login.fxml");
     }
 
+    /**
+     * Performs register button action and displays result of action
+     * @param actionEvent JavaFX ActionEvent object representing the event of the button press
+     */
     @Override
     public void registerButtonAction(ActionEvent actionEvent) {
         clearResultText();
@@ -32,6 +47,11 @@ public class RegisterPresenter implements IRegisterPresenter {
         setResultText(String.valueOf(responseJson.get("result")), String.valueOf(responseJson.get("status")));
     }
 
+    /**
+     * Sets the result of the action given status
+     * @param resultText String object describing the result
+     * @param status String object representing the status of the controller method call
+     */
     @Override
     public void setResultText(String resultText, String status) {
         if (status.equals("error") || status.equals("warning")) {
@@ -51,12 +71,19 @@ public class RegisterPresenter implements IRegisterPresenter {
         TextResultUtil.getInstance().addPseudoClass(status, this.view.getResultTextControl());
     }
 
+    /**
+     * Init method which sets all the button actions
+     */
     @Override
     public void init() {
         this.view.setBackButtonAction(this::backButtonAction);
         this.view.setRegisterButtonAction(this::registerButtonAction);
     }
 
+    /**
+     * Helper method to encode JSON object for registering a user
+     * @return JSONObject object representing a user's registration form
+     */
     @SuppressWarnings("unchecked")
     private JSONObject constructRegisterJson() {
         JSONObject queryJson = new JSONObject();
@@ -74,6 +101,9 @@ public class RegisterPresenter implements IRegisterPresenter {
         return queryJson;
     }
 
+    /**
+     * Helper method to clear all result text and affected form fields
+     */
     private void clearResultText() {
         this.view.setResultText("");
         TextResultUtil.getInstance().removeAllPseudoClasses(this.view.getResultTextControl());
